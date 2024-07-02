551de67670bf837c986a19f6aa0ec51b0bafafbe
rbouserhal <113565977+rbouserhal@users.noreply.github.com>
2024-07-02 14:39:41 -0400

    automated generation of bib files

--- References/merge.sh
+++ References/merge.sh
@@ -1,2 +1,3 @@
-#!/bin/zsh
-cat conferences.bib journals.bib talks.bib > pubs.bib
+#!/bin/sh
+cat conferences.bib journals.bib talks.bib > publications.bib
+/opt/homebrew/Cellar/pandoc/3.2.1/bin/pandoc -t csljson publications.bib -o ../_data/publications.json
