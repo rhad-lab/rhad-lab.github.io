5e4364226ef2736491cc0c03a6ca1348fb5d7229
Pascal Giard <pascal.giard@etsmtl.ca>
2024-07-04 10:17:23 -0400

    Updated script such that it works for me as well (my pandoc is elsewhere, but in my PATH already).

--- References/merge.sh
+++ References/merge.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+export PATH=$PATH:/opt/homebrew/Cellar/pandoc/3.2.1/bin/
 cat conferences.bib journals.bib talks.bib > publications.bib
-/opt/homebrew/Cellar/pandoc/3.2.1/bin/pandoc -t csljson publications.bib -o ../_data/publications.json
+pandoc -t csljson publications.bib -o ../_data/publications.json
