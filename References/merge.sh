e89ad6656d53baca90b1538a547dc08e57ba5c2e
Pascal Giard <pascal.giard@etsmtl.ca>
2024-08-02 10:16:59 -0400

    Document bug of pandoc.

--- References/merge.sh
+++ References/merge.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 export PATH=$PATH:/opt/homebrew/Cellar/pandoc/3.2.1/bin/
+# Create file instead of using pandoc w/ stdin + format specifier (-f bibtex),
+# since there's weird bug where the publisher-place will be missing when used
+# that way. Ideally, that should be filled as a bug at:
+# https://github.com/jgm/pandoc/issues
 cat conferences.bib journals.bib talks.bib > publications.bib
 pandoc -t csljson publications.bib -o ../_data/publications.json
